c3692de81a897e7a2a177d08fa313a0186ed48cb
crasher9032 <tavodash@gmail.com>
2022-11-09 15:01:06 -0600

    cambios

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var sdk         = require("./lib/sdk");
 var config      = require("./config");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 var app    = new Application(null, config);
 var server = new Server(config, app);
-const exp = express();
+var exp = express();
 
 server.start();
 
@@ -22,22 +23,27 @@ sdk.registerBot(require('./LiveChat.js'));
 sdk.registerBot(require('./formulario'));
 
 exp.use(cors());
+exp.use(bodyParser.json());
 
 exp.post('/form', function(req, res){
     try {
         console.log(req.body);
-	context.dataForm = req.body;
+	    context.dataForm = req.body;
         sdk.saveData(req.body.id, req.body)
         .then(function() {
             //Finished
             data.success = 'true';
-            callback(null, 'Terminado');
+            res.header("Access-Control-Allow-Origin","*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Referrer-Policy","origin-when-cross-origin, strict-origin-when-cross-origin");
+            res.header("Content-Security-Policy","default-src 'none'");
             res.send({
                 "status": "success",
             });
+            callback(null, 'Terminado');
         });
     } catch (error) {
-        res.sendStatus(404, error);
+        res.sendStatus(404, {"error" : error});
     }
 });
 
