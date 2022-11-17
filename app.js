c6d643fdd6c1dd8960e2d88804fe6e9548235134
crasher9032 <tavodash@gmail.com>
2022-11-17 13:43:46 -0600

    innitial

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var Application = require("./lib/app");
 var Server      = require("./lib/server");
 var sdk         = require("./lib/sdk");
 var config      = require("./config");
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const formulario = require("./formulario");
 
 var app    = new Application(null, config);
 var server = new Server(config, app);
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const universal = require('./formularioUniversal');
 var exp = express();
+
+sdk.checkNodeVersion();
 
 server.start();
 
@@ -21,10 +24,10 @@ sdk.registerBot(require('./BookACab.js'));
 sdk.registerBot(require('./OrderAPizza.js'));
 sdk.registerBot(require('./BotVariables.js'));
 sdk.registerBot(require('./LiveChat.js'));
-sdk.registerBot(formulario);
+//importacion webhook personlizado
+sdk.registerBot(universal);
 
 exp.use(cors());
 exp.use(bodyParser.json());
-exp.use('/form', formulario.form);
-
+exp.use('/universal', universal.form);
 exp.listen(3000);
---
+++ formularioUniversal.js
@@ -0,0 +1,48 @@
+var botId          = "st-5f411293-4e68-571c-8f9c-fbf7f4132f51";
+var botName        = "El Universal";
+var sdk            = require("./lib/sdk");
+var Promise        = sdk.Promise;
+var request        = require("request");
+var config         = require("./config");
+const requestStore = require("./lib/sdk/lib/requestStore");
+const form = require("express").Router();
+
+module.exports = {
+    form,
+    botId   : botId,
+    botName : botName,
+
+    on_user_message : function(requestId, data, callback) {
+        console.log("on user 1");
+            sdk.sendBotMessage(data, callback);
+        },
+    on_bot_message  : function(requestId, data, callback) {
+    console.log("on user 2");
+        sdk.sendUserMessage(data, callback);
+    },
+    on_webhook      : function(requestId, data, componentName, callback) {
+        console.log("llamada al webhook");
+        try{
+            var context = data.context;
+            if (componentName === 'get_formulario') {
+                console.log(context.contextId);
+                context.url = 'http://ec2-54-164-250-93.compute-1.amazonaws.com/Sueltos/formUniversalPago.html?task=' + context.contextId;
+                callback(null, data);
+            } 
+            if (componentName === 'open_formulario'){
+                callback(null, new sdk.AsyncResponse());
+                let dir = context.contextId;
+                form.post(`/:${dir}`, function(req, res){
+                    try {
+                        context.dataForm = req.body;
+                        sdk.respondToHook(data);
+                    } catch (error) {
+                        res.send(error);
+                    }
+                });
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+};
